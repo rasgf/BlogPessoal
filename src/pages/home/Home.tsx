2428ff5a3b72b9e00e1205971f1faf538d0c5d6c
Ricardo <pattz.rg@hotmail.com>
2023-11-13 02:25:57 -0300

    introduzindo as funções de cadastro e login de novos usuários

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,66 @@
+function Home() {
+    
+  return (
+      <>
+          <div className="bg-indigo-900 flex justify-center">
+              <div className='container grid grid-cols-2 text-white'>
+                  <div className="flex flex-col gap-4 items-center justify-center py-4">
+                  <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+                      <p className='text-xl'>
+                          Expresse aqui seus pensamentos e opniões
+                      </p>
+
+                      <div className="flex justify-around gap-4">
+                          <div className='rounded text-white 
+                                          border-white border-solid border-2 py-2 px-4'
+                              >
+                              Nova Postagem
+                          </div>
+                      </div>
+                  </div>
+
+                  <div className="flex justify-center ">
+                      <img
+                          src="https://i.imgur.com/fyfri1v.png"
+                          alt="Imagem Página Home"
+                          className='w-2/3'
+                      />
+                  </div>
+              </div>
+          </div>
+      </>
+  )
+}
+
+export default Home
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './Home.css'
+// import homeLogo from '../../assets/sukuna.jpeg'
+// function home() {
+//   return (
+//     <>
+//     <div className="w-screen flex justify-center">
+//         <div className="max-w-7xl flex flex-col items-center">
+//             <h1 className ='titulo'>PRIMEIRO PROJETO REACT AUEHUAHEU</h1>
+//             <img src={homeLogo} className='img' alt="Imagem tela inicial"/>
+//         </div>
+//     </div>
+//     </>
+//   )
+// }
+
+// export default home
