42c7e0cd288a955ac87f9921f05a378599634231
Ricardo <pattz.rg@hotmail.com>
2023-11-21 14:38:51 -0300

    ajeitando classe JwtAuthFilter para o deploy

--- src/main/java/com/generation/blogpessoal/security/JwtAuthFilter.java
+++ src/main/java/com/generation/blogpessoal/security/JwtAuthFilter.java
@@ -21,57 +21,43 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
-/*Essa classe faz uma serie de filtros e validações baseado nas variáeis que declaramos ali.
- * os headers é um metadado para capturar nas requisições. É um parametro que vem pelo cabeçario da infomação.
- * Através pel JSON.
- * 
- * 
- * */
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
-	
-	@Autowired
-	private JwtService jwtService;
-	
-	@Autowired
-	private UserDetailsServiceImpl userDetailsService;
-	
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-										FilterChain filterChain) throws ServletException, IOException, java.io.IOException{
-		
-		/*captura o token e faz os teste para validações.*/
-		String authHeader = request.getHeader("Authorization");
-		String token = null;
-		String username = null;
-		
-		try {
-			if(authHeader != null && authHeader.startsWith("Bearer ")) {
-				token = authHeader.substring(7);
-				username = jwtService.extractUsername(token);
-			}
-			
-			/*Verifica o username e através do token cria o objeto userDetails.*/
-			if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-				UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-				
-				//Ocorre a autenticação do token.
-				if (jwtService.validateToken(token, userDetails)) {
-					UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-					authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-					SecurityContextHolder.getContext().setAuthentication(authToken);
-				}
-			}
-			//Compartilha a informação do nosso token para o restante do código, e o catch é as tratativa de erro.
-			
-			filterChain.doFilter(request, response);
-			
-		} catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException | ResponseStatusException e) {
-			response.setStatus(HttpStatus.FORBIDDEN.value());
-			return;
-		}
-		
-	}
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
+    
+        try{
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                token = authHeader.substring(7);
+                username = jwtService.extractUsername(token);
+            }
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    
+                if (jwtService.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            
+            }
+            filterChain.doFilter(request, response);
+
+        }catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException 
+                | SignatureException | ResponseStatusException e){
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            return;
+        }
+    }
 }
