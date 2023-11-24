9ff98fd442cce6052ba994c94ee0a353da90fec1
Ricardo <pattz.rg@hotmail.com>
2023-11-24 13:28:56 -0300

    adicionando classe login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,41 @@
-function Navbar() {
-    return (
-        <>
-            <div className='w-full bg-indigo-900 text-white
-                flex justify-center py-4'>
-            
-                <div className="container flex justify-between text-lg">
-                    Blog Pessoal
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-                    <div className='flex gap-4'>
-                        Postagens
-                        Temas
-                        Cadastrar tema
-                        Perfil
-                        Sair
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+import { AuthContext } from "../../assets/contexts/AuthContext";
+
+function Navbar() {
+  const navigate = useNavigate();
+  const { handleLogout } = useContext(AuthContext);
+
+  function logout() {
+    handleLogout();
+    alert("Usuário deslogado com sucesso");
+    navigate("/login");
+  }
+  return (
+    <>
+      <div
+        className="w-full bg-indigo-900 text-white
+                flex justify-center py-4"
+      >
+        <div className="container flex justify-between text-lg">
+          <Link to="/home" className="text-2xl font-bold">
+            Blog Pessoal
+          </Link>
+
+          <div className="flex gap-4">
+            Postagens
+            <Link to="/temas" className="hover:underline">Temas</Link>
+            Cadastrar tema 
+            Perfil
+            <Link to="" onClick={logout} className="hover:underline">
+              Sair
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
